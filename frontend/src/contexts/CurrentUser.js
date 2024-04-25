3c42160df1c47d4481c34a3098fc2cb92a8cf07a
eachoat <eachoat@gmail.com>
2024-04-25 19:06:50 -0400

    4/25 work

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -2,28 +2,23 @@ import React, { createContext, useState, useEffect } from "react";
 
 export const CurrentUserContext = createContext();
 
-function CurrentUserProvider({ children }) {
-    const [currentUser, setCurrentUser] = useState(null);
 
-    useEffect(() => {
-        const getLoggedInUser = async () => {
-            try {
-                const response = await fetch('http://localhost:5000/authentication/profile');
-                if (response.ok) {
-                    const user = await response.json();
-                    setCurrentUser(user);
-                } else {
-                    // Handle error response
-                    console.error('Error fetching user data:', response.status, response.statusText);
-                }
-            } catch (error) {
-                // Handle fetch error
-                console.error('Fetch error:', error);
+    function CurrentUserProvider({ children }) {
+    
+        const [currentUser, setCurrentUser] = useState(null)
+        useEffect(() => {
+    
+            const getLoggedInUser = async () => {
+                let response = await fetch('http://localhost:5000/authentication/profile', {
+                credentials: 'include'
+            })
+                let user = await response.json()
+                setCurrentUser(user)
             }
-        };
-
-        getLoggedInUser();
-    }, []);
+            getLoggedInUser()
+        }, [])
+      
+    
 
     return (
         <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
